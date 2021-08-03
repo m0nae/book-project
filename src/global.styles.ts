443060c347eff0698bdabdac9f1c194c4389327b
A. Monae Payne <amonaepayne@gmail.com>
2021-08-02 23:30:20 -0500

    :wrench::construction: add css variables
    I haven't figured out a naming convention that I want to stick to, so
    they don't make much sense now.

--- src/global.styles.ts
+++ src/global.styles.ts
@@ -1,4 +1,16 @@
 import styled from "styled-components";
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  html {
+    --small-text-size: 0.7rem;
+--small-text-spacing: 2px;
+--small-text-color: grey;
+--small-text-weight: 600;
+--small-text-transform: uppercase;
+--dark-red: #cc4646;
+  }
+`;
 
 export const Avatar = styled.img`
   height: 60px;
