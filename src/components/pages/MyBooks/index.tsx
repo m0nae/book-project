405fa05e1126bc7431f129b36debd3bbee6edc3f
A. Monae Payne <amonaepayne@gmail.com>
2021-08-07 23:07:07 -0500

    :sparkles: implement pagination logic

--- src/components/pages/MyBooks/index.tsx
+++ src/components/pages/MyBooks/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback, useMemo, useRef, useEffect } from "react";
 import {
   Popover,
   PopoverTrigger,
@@ -9,88 +9,79 @@ import {
   PopoverArrow,
   PopoverCloseButton,
 } from "@chakra-ui/react";
+import ReactPaginate from "react-paginate";
 import Header from "../../Header";
+import Option from "components/Option";
 import { LargeBookCover } from "../Profile/index.styles";
 import { Main, Sidebar, Wrapper } from "./index.styles";
 
+import { readingLists as READINGLISTS } from "src/dummyData";
+import icon from "components/icons";
+
 export default function MyBooks() {
-  // these are for all of the "reading lists".
-  // if i give users the ability to create their own, i might just make a single state
-  // and pull the data from an api. maybe useReducer would be better,
-  // but for now im hard-coding the reading lists like this
-  const bookOne = {
-    img: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1298417783l/23425.jpg",
-  };
-
-  type GenericArray = Array<Object | null>;
-
-  const ALLBOOKS: GenericArray = [];
-  const CURRENTLYREADINGBOOKS: GenericArray = [];
-  const WANTTOREADBOOKS: GenericArray = [];
-
-  const printBooks = (arr: GenericArray, book: Object, num: Number) => {
-    for (let i = 0; i < num; i++) {
-      arr.push(book);
-    }
-  };
-
-  printBooks(ALLBOOKS, bookOne, 9);
-  printBooks(CURRENTLYREADINGBOOKS, bookOne, 3);
-  printBooks(WANTTOREADBOOKS, bookOne, 15);
-
-  const [allBooks, setAllBooks] = useState(ALLBOOKS);
-  const [wantToReadBooks, setWantToReadBooks] = useState(WANTTOREADBOOKS);
-  const [currentlyReadingBooks, setCurrentlyReadingBooks] = useState(
-    CURRENTLYREADINGBOOKS
-  );
-
-  const allBooksView = ALLBOOKS.map((book: any) => <Book src={book.img} />);
-  const currentlyReadingBooksView = CURRENTLYREADINGBOOKS.map((book: any) => (
-    <Book src={book.img} />
-  ));
-  const wantToReadBooksView = WANTTOREADBOOKS.map((book: any) => (
-    <Book src={book.img} />
-  ));
-
+  const [readingLists, setReadingList] = useState(READINGLISTS);
   const [selectedReadingList, setSelectedReadingList] = useState("all");
+  const [currentPage, setCurrentPage] = useState(0);
 
-  const readingListView = useCallback(() => {
-    switch (selectedReadingList) {
-      case "all":
-        return allBooksView;
-      case "currently-reading":
-        return currentlyReadingBooksView;
-      default:
-        return allBooksView;
-    }
-  }, [selectedReadingList]);
+  const READINGLISTVIEWS = useMemo(() => {
+    let readingList: any = {};
+    readingLists.forEach((list) => {
+      readingList[list.name] = {
+        ...list,
+        books: list.books.map((book: any) => <Book src={book.img} />),
+      };
+    });
 
-  const readingListTitle = useCallback(() => {
-    switch (selectedReadingList) {
-      case "all":
-        return "My Books";
-      case "currently-reading":
-        return "Currently Reading";
-      default:
-        return "My Books";
-    }
-  }, [selectedReadingList]);
+    return readingList;
+  }, [readingLists]);
 
-  // function readingListView() {
-  //   switch(selectedReadingList) {
-  //     case 'all':
-  //       return allBooksView;
-  //     case 'currently-reading':
-  //       return currentlyReadingBooksView;
-  //     default:
-  //       return allBooksView;
-  //   }
-  // }
+  const readingListView = useMemo(() => {
+    return READINGLISTVIEWS[selectedReadingList].books;
+  }, [readingLists, selectedReadingList]);
+
+  const readingListTitle = useMemo(() => {
+    return READINGLISTVIEWS[selectedReadingList].title;
+  }, [readingLists, selectedReadingList]);
+
+  const allReadingLists = readingLists.map((list) => (
+    <Option
+      selectedOption={selectedReadingList}
+      setSelectedOption={setSelectedReadingList}
+      radioName="reading-list"
+      optionName={list.name}
+      optionNameLabel={list.title}
+    />
+  ));
+
+  const getPaginatedData = useMemo(() => {
+    const data = readingListView;
+    const dataLimit = 30;
+    const startIndex =
+      (currentPage === 0 ? currentPage + 1 : currentPage) * dataLimit -
+      dataLimit;
+    const endIndex = startIndex + dataLimit;
+    return data.slice(startIndex, endIndex);
+  }, [readingListView, currentPage]);
+
+  // scrolls to the top of page when reading list changes
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [readingListView, currentPage]);
+
+  useEffect(() => {
+    setCurrentPage(0);
+  }, [readingListView]);
+
+  const showPagination = getPaginatedData.length > 29 || currentPage > 0;
 
   return (
     <>
       <Header />
       <Wrapper>
+        <Sidebar>{allReadingLists}</Sidebar>
+        <Main>
+          <h1>{readingListTitle}</h1>
+          <div>{getPaginatedData}</div>
           {showPagination && (
             <ReactPaginate
               forcePage={currentPage}
@@ -107,15 +98,8 @@ export default function MyBooks() {
               previousLabel={<icon.LeftChevron />}
               nextLabel={<icon.RightChevron />}
               onPageChange={(pg: any) => setCurrentPage(pg.selected)}
-          />
-          <label htmlFor="currently-reading">Currently Reading</label>
-          <p>Read</p>
-          <p>Want to Read</p>
-          <p>Favorites</p>
-        </Sidebar>
-        <Main>
-          <h1>{readingListTitle()}</h1>
-          <div>{readingListView()}</div>
+            />
+          )}
         </Main>
       </Wrapper>
     </>
@@ -129,7 +113,13 @@ interface BookProps {
 function Book({ src }: BookProps) {
   return (
     <>
-      <Popover placement="right" trigger="hover" openDelay={100} arrowSize={20}>
+      <Popover
+        placement="right"
+        trigger="hover"
+        openDelay={100}
+        arrowSize={20}
+        isLazy
+      >
         <PopoverTrigger>
           <LargeBookCover style={{ cursor: "pointer" }} src={src} />
         </PopoverTrigger>
